1c74ba69ac61a248dc15411bfc26e611020be8d0
Samuel Rocha <darocha.vasques@gmail.com>
2022-10-11 22:45:12 -0300

    +3 exercicios

---
+++ aula6/mario_less/mario.py
@@ -0,0 +1,14 @@
+from cs50 import get_int
+
+tam = 0
+while True:
+    tam = get_int("Height: ")
+    if tam >= 1 and tam <= 8:
+        for i in range(tam):
+            for j in range(tam):
+                if (i + j) >= tam - 1:
+                    print("#", end="")
+                else:
+                    print(" ", end="")
+            print()
+        break
